50985d391c999c77a970772adbb7fced83432036
Furkan Turan <60838036+appsectr@users.noreply.github.com>
2025-04-14 22:47:08 +0300

    Update Q103.jsx

--- Q103.jsx
+++ Q103.jsx
@@ -1,22 +1,23 @@
 const handleLogin = (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    authenticateUser(username, password)
-        .then((response) => {
-            const accessToken = response.data.accessToken;
-            localStorage.setItem("authToken", accessToken);
-            setSessionToken(accessToken);
+  authenticateUser(username, password)
+    .then((response) => {
+      const accessToken = response.data.accessToken;
+      localStorage.setItem("authToken", accessToken);
+      setSessionToken(accessToken);
 
-            const params = new URLSearchParams(window.location.search);
-            const next = params.get('next');
+      const params = new URLSearchParams(window.location.search);
+      const next = params.get('next');
 
-            if (next) {
-                window.location.href = next;
-            } else {
-                window.location.href = "/home";
-            }
-        })
-        .catch(() => {
-            setLoginError("Login failed");
-        });
+      if (next) {
+        window.location.href = next;
+      } else {
+        window.location.href = "/home";
+      }
+    })
+    .catch(() => {
+      setLoginError("Login failed");
+    });
 };
+
