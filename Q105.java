7ee20d4c3ac33e8555f4a80e059da3a48a56e539
Furkan Turan <60838036+appsectr@users.noreply.github.com>
2025-04-27 18:21:04 +0300

    Update Q105.java

--- Q105.java
+++ Q105.java
@@ -1,23 +1,13 @@
-public void importUserAccessPolicies(Menu menu) throws IOException {
+public void importUserAccessPolicies(InputStream jsonInputStream, Menu menu) throws IOException {
     try {
-        var path = consoleIO.readResponse("Select a JSON file containing user access policies:");
-        if (path.isBlank()) {
-            this.consoleIO.printLn("Please enter a valid JSON file path.");
-            menu.accessPolicyMenuOptions();
-            return;
-        }
-        String json = String.join("\n", Files.readAllLines(Path.of(path)));
         ObjectMapper objectMapper = new ObjectMapper();
-        List<UserAccessPolicyDTO> accessPolicyList = objectMapper.readValue(json, new TypeReference<>() {});
+        List<UserAccessPolicyDTO> accessPolicyList = objectMapper.readValue(jsonInputStream, new TypeReference<>() {});
         for (UserAccessPolicyDTO policy : accessPolicyList) {
             this.accessPolicyService.addPolicy(policy);
         }
-    } catch (NoSuchFileException e) {
-        this.consoleIO.printLn("Unable to find the specified JSON file.");
-        menu.accessPolicyMenuOptions();
     } catch (Exception e) {
-        this.consoleIO.printLn("The file contains invalid JSON or corrupted data.");
-        menu.accessPolicyMenuOptions();
+        consoleIO.printLn("Error importing policies.");
     }
+
     menu.accessPolicyMenuOptions();
 }
